93ce2a2ce0b2efc15e408eb081b24d29e9c0d9c9
Nihal Abdul Khader MP <84581207+Nihalmp45@users.noreply.github.com>
2025-03-13 01:36:29 +0530

    color issue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,9 @@ module.exports = {
       "./components/**/*.{js,ts,jsx,tsx}",
       "./app/**/*.{js,ts,jsx,tsx}", // Add this for Next.js 13+ with /app router
     ],
-    darkMode: 'media', // 'media' enables automatic dark mode based on system preference
     theme: {
-      extend: {
-        colors: {
-          background: 'var(--background)',
-          foreground: 'var(--foreground)',
-        },
-      },
+      extend: {},
     },
     plugins: [],
   };
-  
   
